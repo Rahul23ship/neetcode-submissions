bf5923e32e19cec2292c734cb7d083ae121fdf35
Rahul23ship <62433844+Rahul23ship@users.noreply.github.com>
2026-06-01 14:50:58 +0530

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,22 @@
+class TimeMap {
+public:
+    map<string,map<int,string>>mp;
+    TimeMap() {
+
+    }
+    void set(string key, string value, int timestamp) {
+        mp[key][timestamp]=value;
+    }
+    
+    string get(string key, int timestamp) {
+        if(mp.find(key)!=mp.end()){
+            auto& inner=mp[key];
+            auto it=inner.lower_bound(timestamp);
+            if(it!=inner.end() && it->first==timestamp)
+            return it->second;
+            if(it!=inner.begin())it--;
+            if(it->first<timestamp)return it->second;
+        }
+        return "";
+    }
+};
