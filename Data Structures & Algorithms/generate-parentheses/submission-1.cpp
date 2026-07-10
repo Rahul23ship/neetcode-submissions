980639284ecf98da59d200b0665c425eb974300f
Rahul23ship <62433844+Rahul23ship@users.noreply.github.com>
2026-07-10 11:45:24 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        create(ans,n,0,0,"");
+        return ans;
+    }
+    void create(vector<string>&ans,int n, int i,int j,string s){
+        if(j>i)return;
+        if(i==n){
+            while(j<n){
+                s+=")";
+                j++;
+            }
+            ans.push_back(s);
+            return;
+        }
+        if(i<n)
+        create(ans,n,i+1,j,s+"(");
+        if(j<i)
+        create(ans,n,i,j+1,s+")");
+    }
+};
